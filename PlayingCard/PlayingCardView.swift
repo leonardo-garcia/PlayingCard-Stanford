6766154ab4a1cbd24f18e970882dad278fa8900b
Leonardo Garcia <leonardo.garcia@wizeline.com>
2018-08-21 21:29:35 -0500

    Draw card view border

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -9,23 +9,29 @@
 import UIKit
 
 class PlayingCardView: UIView {
+    
+    var rank: Int = 5 { didSet{ setNeedsDisplay(); setNeedsLayout() } }
+    var suit: String = "♥️" { didSet{ setNeedsDisplay(); setNeedsLayout() } }
+    var isFaceUp: Bool = true { didSet{ setNeedsDisplay(); setNeedsLayout() } }
+ 
+    private var cornerString: NSAttributedString {
+        return centeredAttributedString("\(rank)" + "\n" + suit, fontSize: 0.0)
+    }
+    
+    private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString {
+        
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        return NSAttributedString(string: string, attributes: [NSAttributedStringKey.paragraphStyle : paragraphStyle, .font: font])
+    }
 
     override func draw(_ rect: CGRect) {
-//        if let context = UIGraphicsGetCurrentContext() {
-//            context.addArc(center: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-//            context.setLineWidth(5.0)
-//            UIColor.green.setFill()
-//            UIColor.red.setStroke()
-//            context.strokePath()
-//            context.fillPath()
-//        }
-        let path = UIBezierPath()
-        path.addArc(withCenter: CGPoint(x: bounds.midX, y: bounds.midY), radius: 100.0, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
-        path.lineWidth = 5.0
-        UIColor.green.setFill()
-        UIColor.red.setStroke()
-        path.stroke()
-        path.fill()
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        roundedRect.addClip()
+        UIColor.white.setFill()
+        roundedRect.fill()
     }
 }
 
