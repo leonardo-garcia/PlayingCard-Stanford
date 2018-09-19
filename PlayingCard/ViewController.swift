f656326c8cf52b0f0ee73af50d8a822d489cb3e1
Leonardo Garcia <leo.equih@gmail.com>
2018-09-18 23:14:42 -0500

    Draw card images and add gesture recognizers

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -12,15 +12,36 @@ class ViewController: UIViewController {
 
     var deck = PlayingCardDeck()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        for _ in 1...10 {
-            if let card = deck.draw() {
-                print("\(card)")
-            }
+    @IBOutlet weak var playingCardView: PlayingCardView! {
+        didSet {
+            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
+            swipe.direction = [.left, .right]
+            playingCardView.addGestureRecognizer(swipe)
+            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(PlayingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
+            playingCardView.addGestureRecognizer(pinch)
         }
     }
+    
+    @objc func nextCard() {
+        if let card = deck.draw() {
+            playingCardView.rank = card.rank.order
+            playingCardView.suit = card.suit.rawValue
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+    }
+
+    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
+        switch sender.state {
+        case .ended:
+            playingCardView.isFaceUp = !playingCardView.isFaceUp
+        default:
+            break
+        }
+    }
     
 }
 
